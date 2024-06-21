346679f3afd9fcc7af5b91e7af40b8f8d96a47bd
LuckyRads <luckyrads@gmail.com>
2024-06-22 02:54:15 +0300

    UPD: sample.

--- secugen/src/Source/token.c
+++ secugen/src/Source/token.c
@@ -1,8 +1,23 @@
 #include "../Include/token.h"
+
 #include <emscripten.h>
+#include <string.h>
 
 EMSCRIPTEN_KEEPALIVE
 int add_numbers(int a, int b)
 {
     return a + b;
+}
+
+char *generate_token()
+{
+    const char *token = "123456789";
+    char *p_token = (char *)malloc(strlen(token) + 1);
+    strcpy(p_token, token);
+    return p_token;
+}
+
+void free_ptr(void *ptr)
+{
+    free(ptr);
 }
